252f2002248f51ec86a2c71fa9fb261434640f08
Pedro <pedrocpt2001@gmail.com>
2021-05-06 22:24:42 -0400

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Pedro Pagán on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
