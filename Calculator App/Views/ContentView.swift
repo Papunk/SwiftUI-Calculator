f8ed41cce8d9153de24fc8af40e019998ff7a3b6
Pedro <pedrocpt2001@gmail.com>
2021-07-01 21:06:11 -0400

    minor changes

--- Calculator App/Views/ContentView.swift
+++ Calculator App/Views/ContentView.swift
@@ -15,14 +15,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-//            CalculatorDisplay(screenWidth: screenWidth, screenHeight: screenHeight, expression: $exp).padding()
-            Text(exp.data.isEmpty ? "hello!" : exp.data)
-                .frame(width: screenWidth - 15, height: screenHeight / 4, alignment: .center)
-                .background(Color.gray.opacity(0.1))
-                .font(Font.title).colorInvert()
-                .clipShape(RoundedRectangle(cornerSize: CGSize.init(width: 35, height: 35)))
-                .overlay(RoundedRectangle(cornerSize: .init(width: 35, height: 35)).stroke(lineWidth: 3)).colorInvert()
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            CalculatorDisplay(screenWidth: screenWidth, screenHeight: screenHeight, expression: $exp).padding()
             ButtonLayout(screenWidth: screenWidth, expression: $exp)
         }
     }
