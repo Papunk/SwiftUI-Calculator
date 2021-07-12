f3f7ed64272ad65e4277788eb5fd3e89a1baacd3
Pedro <pedrocpt2001@gmail.com>
2021-07-11 23:25:25 -0400

    save point

--- Calculator App/Views/CalculatorDisplay.swift
+++ Calculator App/Views/CalculatorDisplay.swift
@@ -19,7 +19,7 @@ struct CalculatorDisplay: View {
         Text(expression.isEmpty ? "hello!" : expression)
             .frame(width: screenWidth - 15, height: screenHeight / 4, alignment: .center)
             .background(currentTheme.dispColor)
-            .font(Font.title).colorInvert() // change this
+            .font(Font.title).colorInvert() // <- change this
             .clipShape(RoundedRectangle(cornerSize: CGSize.init(width: 35, height: 35)))
             .overlay(RoundedRectangle(cornerSize: .init(width: 35, height: 35)).stroke(lineWidth: 3)).colorInvert()
             .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
